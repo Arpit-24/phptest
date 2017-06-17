a0f098d708917072f51024d6497d79f610ffc5c9
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2017-06-17 18:08:55 +0530

    Update testnew.php

--- testnew.php
+++ testnew.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-echo "My first PHP script!";
-?>
+$servername = "mysql4.gear.host";
+$username = "dbphptest";
+$password = "Ow8yj82l~?c5";
 
-</body>
-</html>
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
